6d8339d9df472eb60d678c06cf9312235a118909
Destin Floyd <destin.b.floyd@gmail.com>
2019-04-09 11:34:32 -0400

    heroku deplyment spun up react

---
+++ music_app/urls.py
@@ -0,0 +1,12 @@
+
+# from django.urls import path, include
+# from . import views
+# from rest_framework import routers
+
+# router = routers.DefaultRouter()
+# router.register('Test', views.UserView)
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+   
+# ]
